cac1d66383156404cf51f409cc091c84da2859fe
will11600 <wb11600@gmail.com>
2026-01-04 21:01:01 +0000

    Fix: node fields not showing in editor

---
+++ Source/ProceduralGraphTerrain/Interface/TerrainEditor.cs
@@ -0,0 +1,19 @@
+﻿using FlaxEditor.CustomEditors;
+using FlaxEngine;
+using ProceduralGraph.Interface;
+
+namespace ProceduralGraph.Terrain.Interface;
+
+[CustomEditor(typeof(FlaxEngine.Terrain))]
+internal sealed class TerrainEditor : ProceduralActorEditor
+{
+    public override void Initialize(LayoutElementsContainer layout)
+    {
+        base.Initialize(layout);
+        if (TryFindNode(out IGraphNode? node))
+        {
+            var group = layout.Group("Procedural Generation");
+            group.Property("Models", node.ValueContainer);
+        }
+    }
+}
