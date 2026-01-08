bb9f0d54ea62bd895d4119f48fd235edfe3da9c9
will11600 <wb11600@gmail.com>
2026-01-08 05:00:50 +0000

    Feat: Add terrain regeneration button to editor

--- Source/ProceduralGraphTerrain/Interface/TerrainEditor.cs
+++ Source/ProceduralGraphTerrain/Interface/TerrainEditor.cs
@@ -10,10 +10,19 @@ internal sealed class TerrainEditor : ProceduralActorEditor
     public override void Initialize(LayoutElementsContainer layout)
     {
         base.Initialize(layout);
-        if (TryFindEntity(out IGraphEntity? entity))
+
+        if (!TryFindEntity(out IGraphEntity? entity))
         {
-            var group = layout.Group("Procedural Generation");
-            group.Property("Components", entity.ValueContainer);
+            return;
+        }
+
+        var group = layout.Group("Procedural Generation");
+        group.Property("Components", entity.ValueContainer);
+
+        if (entity is TerrainEntity terrainEntity)
+        {
+            var button = layout.Button("Regenerate").Button;
+            button.Clicked += terrainEntity.MarkAsDirty;
         }
     }
 }
